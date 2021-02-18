627b3eaafb2f44e4686dc0c3db53af8a3cc8024c
Nitiphat2 <moozknot@gmail.com>
2021-02-18 17:58:28 +0700

    ผิด

--- 6320502410_lab3_6.c
+++ 6320502410_lab3_6.c
@@ -10,58 +10,50 @@ int main()
             if(i==num&&j==0)
             {
                 p=i;
+                a=p;
+                for(i=0; a>0; i++)
+                {
+                    a=a/10;
+                }
+                int t[i],l,m[i],h,b=0,z;
+                a=p;
+                i--;
+                for(l=i,h=0; l>=0,h<=i; l--,h++)
+                {
+                    m[l] = a%10;
+                    t[h] = a%10;
+                    a = a/10;
+                }
+                for(l=0; l<=i; l++)
+                {
+                    if(m[l]==t[l])
+                    {
+                        b=1;
+                    }
+                    else
+                    {
+                        z=0;
+                    }
+                }
+                if(b==1&&z!=0)
+                {
+                    for(h=0; h<=i; h++)
+                    {
+                        printf("%d",m[h]);
+                    }
+                    r=1;
+                }
+                else
+                {
+                    z=1;
+                }
             }
             else if(num%i==0)
             {
                 j++;
             }
         }
-        if(p==8)
-        {
-            p=0;
-        }
-        if(p==num)
-        {
-            a=p;
-            for(i=0; a>0; i++)
-            {
-                a=a/10;
-            }
-            int t[i],l,m[i],h,b=0;
-            a=p;
-            i--;
-            for(l=i,h=0; l>=0,h<=i; l--,h++)
-            {
-                m[l] = a%10;
-                t[h] = a%10;
-                a = a/10;
-            }
-            for(l=0;l<=i;l++)
-            {
-                if(m[l]==t[l])
-                {
-                    b=1;
-                }
-                else
-                {
-                    b=0;
-                    break;
-                }
-            }
-            if(b==1)
-            {
-                for(h=0;h<=i; h++)
-                {
-                    printf("%d",m[h]);
-                }
-                r=1;
-            }
-            else
-            {
-                num--;
-            }
-        }
-        else
+        if(r!=1)
         {
             num--;
         }
