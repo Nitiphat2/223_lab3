c5a32483a250c05038075452cf2af5de0d9c1bac
Nitiphat2 <moozknot@gmail.com>
2021-02-18 15:23:47 +0700

    554

--- 6320502410_lab3_3.c
+++ 6320502410_lab3_3.c
@@ -3,5 +3,21 @@ int main()
 {
     int x;
     scanf("%d",&x);
-
+    int i,j;
+    for(i=2;x>1;)
+    {
+        if(x%i==0)
+        {
+            x=x/i;
+            printf("%d\n",i);
+            if(x==1)
+            {
+                printf("0");
+            }
+        }
+        else
+        {
+            i++;
+        }
+    }
 }
