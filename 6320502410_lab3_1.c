5d4f1770f86a570b902b07a455da29009fc5ca67
Nitiphat2 <moozknot@gmail.com>
2021-02-18 14:46:44 +0700

    ไฟล1

---
+++ 6320502410_lab3_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int m[5],n[5]={0,0,0,0,0};
+    int i,j,k;
+    int max = 0;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            scanf("%d",&m[j]);
+            n[i]=n[i]+m[j];
+        }
+        if(max < n[i])
+        {
+            max = n[i];
+            k=i;
+        }
+    }
+    printf("%d %d",k+1,max);
+}
