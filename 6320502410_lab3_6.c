31c94fbc00f9ac0d6d553021404d7b0f338cb5c0
Nitiphat2 <moozknot@gmail.com>
2021-02-18 17:01:38 +0700

    ยาวหน่อย

--- 6320502410_lab3_6.c
+++ 6320502410_lab3_6.c
@@ -1,5 +1,53 @@
 #include<stdio.h>
 int main()
 {
-
+    int num,i,j=0,a,p,r=0;
+    scanf("%d",&num);
+    while(r!=1)
+    {
+        for(i=2; i<=num; i++)
+        {
+            if(i==num&&j==0)
+            {
+                p=i;
+            }
+            else if(num%i==0)
+            {
+                j++;
+            }
+        }
+        printf("\n\n.%d",p);
+        if(p==num)
+        {
+            a=p;
+            for(i=0;a>0;i++)
+            {
+                a=a/10;
+            }
+            int t[i],l,m;
+            a=p;
+            i--;
+            for(l=0;l<=i;l++)
+            {
+                 m = (a%10)*pow(10,i);
+                 a = a/10;
+            }
+            printf("%d",m);
+            if(m == num)
+            {
+                printf("%d",m);
+                r=1;
+            }
+            else
+            {
+                num--;
+                j=0;
+            }
+        }
+        else
+        {
+            num--;
+            j=0;
+        }
+    }
 }
