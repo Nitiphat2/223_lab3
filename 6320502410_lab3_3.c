c83a0537fe1001b212a40c2de92484e8056bc084
Nitiphat2 <moozknot@gmail.com>
2021-02-18 15:27:55 +0700

    22222

--- 6320502410_lab3_3.c
+++ 6320502410_lab3_3.c
@@ -3,21 +3,26 @@ int main()
 {
     int x;
     scanf("%d",&x);
-    int i,j;
-    for(i=2;x>1;)
+    if(x>0&&x<=1000)
     {
-        if(x%i==0)
+        printf("\n");
+        int i,j;
+        for(i=2; x>1;)
         {
-            x=x/i;
-            printf("%d\n",i);
-            if(x==1)
+            if(x%i==0)
             {
-                printf("0");
+                x=x/i;
+                printf("%d\n",i);
+                if(x==1)
+                {
+                    printf("0");
+                }
+            }
+            else
+            {
+                i++;
             }
         }
-        else
-        {
-            i++;
-        }
     }
+
 }
